39cbc2137f31fd516798198f2d651c4c4fbddd7c
Paulo Melo <68909672+ph-melo@users.noreply.github.com>
2024-10-13 00:16:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
- // if(html.classList.contains('light')) {
- //   html.classList.remove('light')
- // } else {
- //   html.classList.add('light')
- // }
-
-
-
-  //pegar a tag img
-  const img = document.querySelector("#profile img")
-
-  //substituir a imagem
-  if(html.classList.contains('light')){
-    // se tiver light mode, adicionar a imagem light
-    img.setAttribute('src','./assets/avatar-light.png')
-  } else {
-    //se tiver sem light mode, mantem a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+//function toggleMode() {
+  let light = localStorage.getItem('light') //ponto de amazenamento do navegador
+  const Switch = document.getElementById('switch') //Declarando meu switch a um Objeto
+  const img = document.querySelector("#profile img") //Pegando a tag da imagem
+  
+  // Declarei o Objeto caso tenha 'Light' na classlist (ON)
+  const enableDarkmode = () => {
+    document.body.classList.add('light')
+    localStorage.setItem('light', 'active')
+    img.setAttribute("src", "./assets/avatar-light.png") //altera a imagem
   }
-}
+ 
+  // Declarei o objeto caso nao tenha 'light' na classlist (OFF)
+  const disableDarkmode = () =>{
+    document.body.classList.remove('light')
+    localStorage.setItem('light', null)
+    img.setAttribute("src", "./assets/avatar.png") //mantem a imagem
+    
+  }
+  
+  if(light === "active") enableDarkmode()
+    
+  Switch.addEventListener("click", () => {
+    light = localStorage.getItem('light')
+    light !== "active" ? enableDarkmode() : disableDarkmode()
+  })
